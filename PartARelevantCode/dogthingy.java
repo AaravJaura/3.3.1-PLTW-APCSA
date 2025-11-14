aea0a47cf44d37237482ffb405af882013f34b9d
AaravJaura <jaura7906@mydusd.org>
2025-11-14 13:45:21 -0800

    Added code for Part A

---
+++ PartARelevantCode/dogthingy.java
@@ -0,0 +1,11 @@
+public class dogthingy {
+    public static void main(String[] args) {
+        String [][] dogdaycamp = new String [3][5];
+        dogdaycamp[0][0] = "Bailey";
+        dogdaycamp[0][3] = "Rosie";
+        dogdaycamp[2][2] = "Ruby";
+        dogdaycamp[0][2] = "R";
+        dogdaycamp[2][1] = "R";
+
+    }
+}
